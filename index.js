b92ffcac317a834271fd64255b34b343a9d6d6ef
aipravo <aipravo.kz@gmail.com>
2025-02-07 18:01:48 +0500

    2

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
-app.use(cors());
+app.use(cors({
+	origin: ['https://aipravo.kz'],
+	credentials: true
+}));
 app.use(express.json());
 app.use('/api', router);
 
