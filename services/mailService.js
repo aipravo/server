f3bcda14e18c53fa44ac8e461654d6aed35a1e83
aipravo <aipravo.kz@gmail.com>
2025-07-09 15:59:03 +0500

    Update mailService.js

--- services/mailService.js
+++ services/mailService.js
@@ -8,8 +8,8 @@ class MailService {
 	constructor() {
 		this.transporter = nodemailer.createTransport({
 			host: process.env.EMAIL_HOST,
-			port: Number(process.env.EMAIL_PORT),
-			secure: true, // 465 = secure:true
+			port: 587,
+			secure: false, // 465 = secure:true
 			auth: {
 				user: process.env.EMAIL_USER,
 				pass: process.env.EMAIL_PASS
